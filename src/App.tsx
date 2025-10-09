cd09bd748bf7f739f64d604e4d6ec1916de5d309
Lucas Kraus <lucas-kraus@hotmail.com>
2025-10-09 14:48:38 -0300

    Refactor App component layout to use Tailwind CSS classes for improved styling and update main.tsx to import the new app.css stylesheet.

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className='flex flex-col h-screen'>
+      <div className='flex'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
