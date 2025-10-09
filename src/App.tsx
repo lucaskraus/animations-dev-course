47eb0343e52232242b77f5c1ce49b22538740f86
Lucas Kraus <lucas-kraus@hotmail.com>
2025-10-09 14:55:30 -0300

    Enhance App component styling by centering content and updating title font size

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
 
   return (
-    <div className='flex flex-col h-screen'>
-      <h2>Animations Dev Course</h2>
+    <div className='flex flex-col h-screen items-center p-24'>
+      <h2 className='text-2xl font-bold'>Animations Dev Course</h2>
     </div>
   )
 }
