4e4ec6aebfab4bdb39d3f8eb65ceeef451f9f7df
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-10 19:27:45 -0300

    Enhance TextReveal component with animation effects and update App layout for improved styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ function App() {
 
   return (
     <div className='flex flex-col h-screen items-center p-24 gap-12'>
-      <h1 className='text-3xl font-medium'>Animations Dev Course</h1>
+      <div className="flex flex-col items-center gap-2">
+        <h1 className='text-3xl font-medium'>Animations Dev Course</h1>
+        <p className="text-sm text-gray-500">
+          Exercises from Animations on the Web course by @emilkowalski_
+        </p>
+      </div>
 
-      <div className="flex flex-col items-center w-full">
-        <h3>Text Reveal</h3>
+      <div className="flex flex-row items-center w-full gap-16 justify-center">
+        <div className="rounded-full bg-blue-500 py-2 px-4">
+          <h3 className="text-2xl font-medium text-white">Text Reveal</h3>
+        </div>
         <TextReveal />
       </div>
     </div>
--- src/components/TextReveal.tsx
+++ src/components/TextReveal.tsx
@@ -4,13 +4,15 @@ export default function TextReveal() {
     const [counter, setCounter] = useState(0);
     const WORD = "Hello, world!";
     return (
-        <div className="flex flex-col items-center gap-4">
+        <div className="flex flex-col items-center">
             <div key={counter}>
+            <h1 className="h1-reveal">
                 {WORD.split("").map((char, index) => (
-                    <span key={index} style={{ "--index": index } as React.CSSProperties}>{char}</span>
+                    <span key={index} className="span-reveal" style={{ "--index": index } as React.CSSProperties}>{char}</span>
                 ))}
+                </h1>
             </div>
-            <button className="button-text-reveal" onClick={() => setCounter(counter + 1)}>
+            <button className="button-reveal" onClick={() => setCounter(counter + 1)}>
                 Replay animation
             </button>
         </div>
