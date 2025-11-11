efec2e053550348b5fc9efca17d4e4d42d9980d6
Lucas Kraus <lucas-kraus@hotmail.com>
2025-11-11 16:31:16 -0300

    Update base path in vite.config.ts and adjust asset paths in index.html for deployment consistency

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/animations-dev-course/',
   plugins: [
     react({
       babel: {
